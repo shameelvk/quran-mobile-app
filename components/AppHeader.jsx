5607b0baf4cb9ed6464edc1ef0dfa068c9d903f2
shameelvk <shameel3311@gmail.com>
2025-11-10 20:20:16 +0530

    refactor:AppHeader

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -3,20 +3,31 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
-export default function AppHeader({ title = "Quran App", showSearch = true }) {
+export default function AppHeader({
+  title = "Quran App",
+  showSearch = true,
+  showMenu = false,
+}) {
   const navigation = useNavigation();
 
   return (
     <View style={styles.header}>
+      {showMenu ? (
+        <TouchableOpacity>
+          <Ionicons name="menu" size={24} color="#A19CC5" />
+        </TouchableOpacity>
+      ) : (
+        <View style={{ width: 24 }} />
+      )}
+
+      <Text style={styles.headerTitle}>{title}</Text>
       {showSearch ? (
         <TouchableOpacity onPress={() => navigation.navigate("Search")}>
-          <Ionicons name="search" size={24} color="#2E7D32" />
+          <Ionicons name="search" size={24} color="#A19CC5" />
         </TouchableOpacity>
       ) : (
         <View style={{ width: 24 }} /> // keeps spacing consistent
       )}
-      <Text style={styles.headerTitle}>{title}</Text>
-      <View style={{ width: 24 }} />
     </View>
   );
 }
@@ -28,16 +39,16 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 16,
     paddingVertical: 12,
-    backgroundColor: "#fff",
-    elevation: 4,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
+    backgroundColor: "#1D2233",
+    // elevation: 4,
+    // shadowColor: "#000",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 3,
   },
   headerTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
-    color: "#2E7D32",
+    color: "#FFFFFF",
   },
 });
